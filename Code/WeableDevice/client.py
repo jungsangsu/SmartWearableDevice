4ce73b03509c1a7e4b77bf63a1f1ed890f56fbbd
jungsangsu <jungsangsu@gmail.com>
2018-08-26 17:31:22 +0900

    client.py edit

--- Code/WeableDevice/client.py
+++ Code/WeableDevice/client.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python
+
+
+import signal
+import buttonshim
 import socket
 import cv2
 import numpy
 import os
 import sys
-import urllib.request
 import urllib
+import urllib.request
 
+# TTS API INFO
+client_id = "mu69nuz21c"
+client_secret = "pZHIA8b9JzKc6l300Oa3nv91N3VkFKq6ctFOd6bX"
+TCP_IP = '220.67.124.124'
+# TCP_IP ='127.0.0.1'
+TCP_PORT = 30001
+sock = socket.socket()
+sock.connect((TCP_IP, TCP_PORT))
+print('TCP Connection Complete')
 
 def TTS(inputString):
     encText = urllib.parse.quote(inputString)
@@ -21,53 +35,51 @@ def TTS(inputString):
         response_body = response.read()
         with open(soundfile, 'wb') as f:
             f.write(response_body)
-        os.system('omxplayer test.mp3')
+        os.system('mplayer -ao pulse test.mp3')
         print("save TTS file" + soundfile)
     else:
         print("Error Code: " + rescode)
 
 
-# TTS API INFO
-client_id = "mu69nuz21c"
-client_secret = "pZHIA8b9JzKc6l300Oa3nv91N3VkFKq6ctFOd6bX"
-
-# 연결할 서버(수신단)의 ip주소와 port번호
-TCP_IP = '220.67.124.124'
-# TCP_IP ='127.0.0.1'
-TCP_PORT = 30000
-
-# 송신을 위한 socket 준비
-sock = socket.socket()
-sock.connect((TCP_IP, TCP_PORT))
-print('TCP 연결완료')
-# OpenCV를 이용해서 webcam으로 부터 이미지 추출
-
-
-while True:
-    meue = input('1.object 2.text 3.Emotion')
+def main(choice):
+    menu = choice
     capture = cv2.VideoCapture(0)
-    ret, frame = capture.read()  # 사진 한프레임을 읽는 부분
-
-    sock.send(str(meue).encode('utf-8'))  # 옵션을 전송
-
+    ret, frame = capture.read()
+    sock.send(str(menu).encode('utf-8'))
     response = (sock.recv(1024)).decode('utf-8')
     print('{}'.format(response))
-
-    # 추출한 이미지를 String 형태로 변환(인코딩)시키는 과정
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     result, imgencode = cv2.imencode('.jpg', frame, encode_param)
     data = numpy.array(imgencode)
     stringData = data.tostring()
     x = str(len(stringData))
-    # String 형태로 변환한 이미지를 socket을 통해서 전송
     sock.send((x.ljust(16)).encode())
     sock.send(stringData)
-
     textresult = (sock.recv(1024)).decode('utf-8')
     print('{}'.format(textresult))
     capture.release()
+    TTS(textresult)
 
-    # TTS(textresult)
+@buttonshim.on_press(buttonshim.BUTTON_A)
+def button_a(button, pressed):
+    main(1)
 
-sock.close()
-print('연결 끝')
+@buttonshim.on_press(buttonshim.BUTTON_B)
+def button_b(button, pressed):
+    main(2)
+
+@buttonshim.on_press(buttonshim.BUTTON_C)
+def button_c(button, pressed):
+    main(3)
+
+@buttonshim.on_press(buttonshim.BUTTON_D)
+def button_d(button, pressed):
+    textresult = "Not defined"
+    TTS(textresult)
+
+@buttonshim.on_press(buttonshim.BUTTON_E)
+def button_e(button, pressed):
+    textresult = "Not defined"
+    TTS(textresult)
+
+signal.pause()
